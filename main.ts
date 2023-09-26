91f3e9216be0184fb4cdd29797846bd008be6741
Lucas Silveira <130493122+LucasGSSA@users.noreply.github.com>
2023-09-26 19:15:38 +0000

    introducao a ts

---
+++ main.ts
@@ -0,0 +1,11 @@
+let arraizinha = ["lucas","joao","pedro"]
+
+
+let numero: number = 19;
+let nulo: null = null;
+let texto: string = "Lucas";
+let boleano: boolean = true;
+let tipoArray: Array<any> = ["joao","maria"];
+let objeto: object = {nome:"joao"};
+
+console.log(numero);
